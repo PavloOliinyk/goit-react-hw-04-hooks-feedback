b6f3beb9afb5ccdb28809ca9587ea76427c9c503
Pavlo Oliinyk <oliinyk.pavlo@gmail.com>
2021-10-10 14:21:31 +0300

    перенес файлы

---
+++ src/FeedbackOptions/FeedbackOptions.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import s from './FeedbackOptions.module.css';
+
+class FeedbackOptions extends Component {
+  render() {
+    const { options, onLeaveFeedback } = this.props;
+    return (
+      <ul className={`list ${s.feedbackList}`}>
+        {options.map(option => (
+          <li key={option}>
+            <button
+              className={s.btn}
+              type="button"
+              value={option}
+              onClick={() => onLeaveFeedback(option)}
+            >
+              {option}
+            </button>
+          </li>
+        ))}
+      </ul>
+    );
+  }
+}
+
+FeedbackOptions.propTypes = {
+  onLeaveFeedback: PropTypes.func.isRequired,
+  options: PropTypes.arrayOf(PropTypes.oneOf(['good', 'neutral', 'bad'])),
+};
+
+export default FeedbackOptions;
